f5e2bef718f41a55c8c4fd2637eae51a64f25ae2
Alex Carol <alex.carol.c@gmail.com>
2017-06-05 11:51:57 +0200

    Implemented differences of square

---
+++ difference-of-squares/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn square_of_sum(n: i64) -> i64 {
+    let sum = n * (n + 1) / 2;
+
+    sum * sum
+}
+
+pub fn sum_of_squares(n: i64) -> i64 {
+    let mut sum = 0;
+
+    for i in 1..(n + 1) {
+        sum += i * i
+    }
+
+    sum
+}
+
+pub fn difference(n: i64) -> i64 {
+    square_of_sum(n) - sum_of_squares(n)
+}
