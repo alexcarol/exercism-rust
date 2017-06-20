66f8e40f8b3191bd082c1edb0ac128e3ef8444d5
Alex Carol <alex.carol.c@gmail.com>
2017-06-20 14:08:27 +0200

    Simplified word_count

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
-    let mut h = HashMap::new();
+    let mut h: HashMap<String, u32> = HashMap::new();
     let split = s.split(" ");
     for s in split {
         let stringized = String::from(s)
@@ -19,12 +19,7 @@ pub fn word_count(s: &str) -> HashMap<String, u32> {
             continue;
         }
 
-        if h.contains_key(AsRef::<str>::as_ref(&stringized)) {
-            let x = h.get_mut(AsRef::<str>::as_ref(&stringized)).unwrap();
-            *x += 1;
-        } else {
-            h.insert(stringized, 1);
-        }
+        *h.entry(stringized).or_insert(0) += 1;
     }
 
     h
