0a58568067192524a4e5e1435dfda7dba392322b
alexcarol <alex.carol.c@gmail.com>
2017-06-26 23:51:32 +0200

    Use num to implement compatibility for float

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,30 +1,38 @@
-use std::cmp;
-
-pub struct Triangle {
-    sides: [u64; 3],
+extern crate num;
+pub struct Triangle<T> {
+    sides: [T; 3],
 }
 
-impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Result<Triangle, bool> {
-        if !sides.into_iter().fold(
-            true,
-            |acc, length| acc && *length != 0,
-        )
-        {
-            return Result::Err(false);
+impl<T> Triangle<T>
+where
+    T: Copy + PartialOrd + std::ops::Add + num::Zero,
+{
+    pub fn build(sides: [T; 3]) -> Result<Triangle<T>, &'static str> {
+        if sides[0] == num::zero() || sides[1] == num::zero() || sides[2] == num::zero() {
+            return Result::Err("some side equals zero");
         }
 
-        let max_side = sides.into_iter().fold(
-            0,
-            |acc, length| cmp::max(acc, *length),
-        );
-        if max_side * 2 >= sides[0] + sides[1] + sides[2] {
-            return Result::Err(false);
+        let max_side = if sides[0] > sides[1] {
+            if sides[0] > sides[2] {
+                sides[0]
+            } else {
+                sides[2]
+            }
+        } else {
+            if sides[1] > sides[2] {
+                sides[1]
+            } else {
+                sides[2]
+            }
+        };
+
+        let sum = sides[0] + sides[1] + sides[2];
+        if max_side + max_side >= sum {
+            return Result::Err("Max side >= max sum");
         }
 
         Result::Ok(Triangle { sides })
     }
-
     pub fn is_scalene(&self) -> bool {
         !self.is_isosceles() && !self.is_equilateral()
     }
