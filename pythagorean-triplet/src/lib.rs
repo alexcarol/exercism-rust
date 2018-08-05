9bb6081eafa036aa0d1402e7c29b4212233092d3
alexcarol <alex.carol.c@gmail.com>
2018-08-05 03:01:02 +0200

    Simplified solution to avoid sort

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,15 +1,10 @@
 pub fn find() -> Option<u32> {
-    for i in 1..1000 {
-        for j in 1..1000 {
-            if i + j < 1000 {
-                let k = 1000 - i - j;
-                let mut numbers = vec![i, j, k];
-                numbers.sort();
+    for i in 334..999 {
+        for j in 1..(1000 - i) {
+            let k = 1000 - i - j;
 
-                if numbers[0] * numbers[0] + numbers[1] * numbers[1] == numbers[2] * numbers[2] {
-                    println!("{}, {}, {}", i, j, k);
-                    return Option::from(i * j * k);
-                }
+            if i * i == j * j + k * k {
+                return Option::from(i * j * k);
             }
         }
     }
