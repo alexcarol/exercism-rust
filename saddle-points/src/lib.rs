93a83b0c89e714a2f8d710770f348ef01973eeeb
alexcarol <alex.carol.c@gmail.com>
2018-08-08 21:07:24 -0400

    Fix saddle-points to use iterators instead of loops to get the points

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,28 +1,33 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut points =  vec![];
-    for i in 0..input.len() {
-        for j in 0..input[i].len() {
-            if is_saddle_point(i, j, input) {
-                points.push((i, j));
-            }
-        }
-    }
-
-    points
+    input
+        .iter()
+        .enumerate()
+        .flat_map(|(row_index, row)|
+            row
+                .iter()
+                .enumerate()
+                .filter_map(move |(column_index, _cell)|
+                    if is_saddle_point(row_index, column_index, input) {
+                        Some((row_index, column_index))
+                    } else {
+                        None
+                    }
+                )
+        ).collect()
 }
 
-fn is_saddle_point(i: usize, j:  usize, input: &[Vec<u64>]) -> bool {
+fn is_saddle_point(i: usize, j: usize, input: &[Vec<u64>]) -> bool {
     let element = input[i][j];
 
     for k in 0..input[i].len() {
         if element < input[i][k] {
-            return false
+            return false;
         }
     }
 
     for k in 0..input.len() {
         if element > input[k][j] {
-            return false
+            return false;
         }
     }
 
