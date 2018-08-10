07f58124f302cde4235240b60897af6208bde966
alexcarol <alex.carol.c@gmail.com>
2018-08-10 08:43:33 -0400

    saddle-points solution with iterators

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -19,17 +19,14 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
 fn is_saddle_point(i: usize, j: usize, input: &[Vec<u64>]) -> bool {
     let element = input[i][j];
 
-    for k in 0..input[i].len() {
-        if element < input[i][k] {
-            return false;
-        }
-    }
-
-    for k in 0..input.len() {
-        if element > input[k][j] {
-            return false;
-        }
-    }
-
-    true
+    input[i]
+        .iter()
+        .map(|value| element < *value)
+        .filter(|element_less_than_value| *element_less_than_value)
+        .count() <= 0
+        && input
+        .iter()
+        .map(|row| element > row[j])
+        .filter(|is_greater| *is_greater)
+        .count() == 0
 }
